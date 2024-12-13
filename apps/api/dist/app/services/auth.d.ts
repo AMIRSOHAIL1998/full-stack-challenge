c2073994c2cd3b5408bc99114f6a39accd8bcceb
Md Amir Sohail <amir@betalectic.com>
2024-12-14 00:27:20 +0530

    dependancy update

--- apps/api/dist/app/services/auth.d.ts
+++ apps/api/dist/app/services/auth.d.ts
@@ -1,2 +1,2 @@
-export declare const login: (email: string, password: string, type: string) => Promise<any>;
-export declare const signup: (request: any) => Promise<any>;
+export declare const login: (email: string, password: string, type: string) => Promise<string>;
+export declare const signup: (request: any) => Promise<string>;
