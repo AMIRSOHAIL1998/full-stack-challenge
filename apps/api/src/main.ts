db27d7c1d10a344a601152935b237fdc2cbf7bf5
Md Amir Sohail <amir@betalectic.com>
2024-12-14 00:12:40 +0530

    added start command

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -17,6 +17,6 @@ app.use('/users', authMiddleware, userRoutes);
 
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/users/read`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
